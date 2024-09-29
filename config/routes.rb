c76f7d2ddd29894143fe53aacae583668bc6543c
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-09-29 19:19:31 +0000

    Save

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
 
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
+  post("/insert_actor", { :controller => "actors", :action => "create" })
 end
